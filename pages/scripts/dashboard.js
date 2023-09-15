423fe7e831bbcff8da2052ce5ca2ba5ca9759d40
ggrewal99 <guruvindergrewal98@gmail.com>
2023-09-16 00:05:27 +1000

    Add more func to dashboard page

--- pages/scripts/dashboard.js
+++ pages/scripts/dashboard.js
@@ -1,3 +1,76 @@
 const BACKEND_URL = "http://localhost:3000";
 
-document.addEventListener("DOMContentLoaded", function () {});
+document.addEventListener("DOMContentLoaded", function () {
+    const viewBtn = document.querySelector(".view-jobs-btn");
+    const addBtn = document.querySelector(".add-job-btn");
+    const viewJobsDiv = document.querySelector(".view-jobs-div");
+    const addJobDiv = document.querySelector(".add-job-div");
+    const jobCount = document.querySelector(".jobCount");
+    const pendingCount = document.querySelector(".pendingCount");
+    const inProgressCount = document.querySelector(".inProgressCount");
+    const declinedCount = document.querySelector(".declinedCount");
+    const acceptedCount = document.querySelector(".acceptedCount");
+
+    var pendingCounter = 0,
+        inProgressCounter = 0,
+        declinedCounter = 0,
+        acceptedCounter = 0;
+
+    viewBtn.addEventListener("click", function () {
+        addJobDiv.style.display = "none";
+        viewJobsDiv.style.display = "grid";
+        viewBtn.style.backgroundColor = "#1c41a7";
+        addBtn.style.backgroundColor = "#3661d7";
+        console.log(localStorage.getItem("token"));
+        axios
+            .get(`${BACKEND_URL}/api/v1/jobs`, {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${localStorage.getItem("token")}`,
+                },
+            })
+            .then((response) => {
+                if (response.status === 200) {
+                    console.log("OK");
+                    return response.data;
+                } else {
+                    console.log("Error!!");
+                    throw new Error("Something went wrong. Try again later!");
+                }
+            })
+            .then((data) => {
+                console.log(data);
+                jobCount.textContent = data.count;
+                for (let i = 0; i < data.jobs.length; i++) {
+                    if (data.jobs[i].status === "Pending") {
+                        pendingCounter++;
+                    } else if (jobs[i].status === "In progress") {
+                        inProgressCounter++;
+                    } else if (jobs[i].status === "Declined") {
+                        declinedCounter++;
+                    } else {
+                        acceptedCounter++;
+                    }
+                }
+                pendingCount.textContent = pendingCounter;
+                inProgressCount.textContent = inProgressCounter;
+                declinedCount.textContent = declinedCounter;
+                acceptedCount.textContent = acceptedCounter;
+            })
+            .catch((error) => {
+                console.log(localStorage.getItem("token"));
+                if (error.response && error.response.status === 401) {
+                    console.log("Error");
+                } else {
+                    console.log("Something went wrong");
+                }
+            });
+    });
+
+    addBtn.addEventListener("click", function () {
+        addJobDiv.style.display = "flex";
+        viewJobsDiv.style.display = "none";
+        addBtn.style.backgroundColor = "#1c41a7";
+        viewBtn.style.backgroundColor = "#3661d7";
+    });
+});
--- pages/scripts/login.js
+++ pages/scripts/login.js
@@ -1,106 +1,108 @@
 const BACKEND_URL = "http://localhost:3000";
 
 document.addEventListener("DOMContentLoaded", function () {
-	const nameInput = document.querySelector(".form-name");
-	const emailInput = document.querySelector(".form-email");
-	const passwordInput = document.querySelector(".form-password");
-	const confirmPasswordInput = document.querySelector(
-		".form-confirm-password"
-	);
-	const loginBtn = document.querySelector(".btn-log");
-	const registerBtn = document.querySelector(".btn-reg");
-	const msg = document.querySelector(".msg");
+    const nameInput = document.querySelector(".form-name");
+    const emailInput = document.querySelector(".form-email");
+    const passwordInput = document.querySelector(".form-password");
+    const confirmPasswordInput = document.querySelector(
+        ".form-confirm-password"
+    );
+    const loginBtn = document.querySelector(".btn-log");
+    const registerBtn = document.querySelector(".btn-reg");
+    const msg = document.querySelector(".msg");
 
-	loginBtn.addEventListener("click", function (e) {
-		e.preventDefault();
-		const email = emailInput.value.trim();
-		const password = passwordInput.value;
+    loginBtn.addEventListener("click", function (e) {
+        e.preventDefault();
+        const email = emailInput.value.trim();
+        const password = passwordInput.value;
 
-		if (isValidForm(email, password)) {
-			const formData = {
-				email: email,
-				password: password,
-			};
+        if (isValidForm(email, password)) {
+            const formData = {
+                email: email,
+                password: password,
+            };
 
-			axios
-				.post(`${BACKEND_URL}/api/v1/auth/login`, formData, {
-					headers: {
-						"Content-Type": "application/json",
-					},
-				})
-				.then((response) => {
-					if (response.status === 200) {
-						console.log("OK");
-						return response.data;
-					} else {
-						console.log("Error!!");
-						throw new Error(
-							"Something went wrong. Try again later!"
-						);
-					}
-				})
-				.then((data) => {
-					displayMessage("Success!", "green"); // Temporary success message
-				})
-				.catch((error) => {
-					if (error.response.status === 401) {
-						displayMessage("Invalid email or password!");
-					} else {
-						displayMessage(
-							"Something went wrong. Try again later!",
-							"red"
-						);
-					}
-				});
-		}
-	});
+            axios
+                .post(`${BACKEND_URL}/api/v1/auth/login`, formData, {
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                })
+                .then((response) => {
+                    if (response.status === 200) {
+                        console.log("OK");
+                        return response.data;
+                    } else {
+                        console.log("Error!!");
+                        throw new Error(
+                            "Something went wrong. Try again later!"
+                        );
+                    }
+                })
+                .then((data) => {
+                    localStorage.setItem("token", data.token);
+                    displayMessage("Success!", "green"); // Temporary success message
+                    window.location.href = `${BACKEND_URL}/pages/protected-pages/dashboard.html`;
+                })
+                .catch((error) => {
+                    if (error.response.status === 401) {
+                        displayMessage("Invalid email or password!");
+                    } else {
+                        displayMessage(
+                            "Something went wrong. Try again later!",
+                            "red"
+                        );
+                    }
+                });
+        }
+    });
 
-	function isValidForm(email, password) {
-		resetForm();
+    function isValidForm(email, password) {
+        resetForm();
 
-		if (email !== "" && password !== "") {
-			if (checkForErrors(email)) {
-				return false;
-			} else {
-				return true;
-			}
-		} else {
-			displayMessage("Please enter the required fields*", "red");
-			if (email === "") {
-				emailInput.classList.add("form-field-error");
-			}
-			if (password === "") {
-				passwordInput.classList.add("form-field-error");
-			}
-			return false;
-		}
-	}
+        if (email !== "" && password !== "") {
+            if (checkForErrors(email)) {
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            displayMessage("Please enter the required fields*", "red");
+            if (email === "") {
+                emailInput.classList.add("form-field-error");
+            }
+            if (password === "") {
+                passwordInput.classList.add("form-field-error");
+            }
+            return false;
+        }
+    }
 
-	function checkForErrors(email) {
-		if (!isValidEmail(email)) {
-			displayMessage(
-				"Invalid email or password. Please try again.",
-				"red"
-			);
-		}
-	}
+    function checkForErrors(email) {
+        if (!isValidEmail(email)) {
+            displayMessage(
+                "Invalid email or password. Please try again.",
+                "red"
+            );
+        }
+    }
 
-	function isValidEmail(email) {
-		const emailRange =
-			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-		return emailRange.test(email);
-	}
+    function isValidEmail(email) {
+        const emailRange =
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return emailRange.test(email);
+    }
 
-	function displayMessage(message, color) {
-		msg.textContent = message;
-		msg.style.color = color;
-	}
+    function displayMessage(message, color) {
+        msg.textContent = message;
+        msg.style.color = color;
+    }
 
-	function resetForm() {
-		msg.textContent = "";
-		emailInput.value = "";
-		passwordInput.value = "";
-		emailInput.classList.remove("form-field-error");
-		passwordInput.classList.remove("form-field-error");
-	}
+    function resetForm() {
+        msg.textContent = "";
+        emailInput.value = "";
+        passwordInput.value = "";
+        emailInput.classList.remove("form-field-error");
+        passwordInput.classList.remove("form-field-error");
+    }
 });
